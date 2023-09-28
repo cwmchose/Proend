0a837db31800fd2267d42c50cd97347dd0c44d8d
Coleman McHose <cwmchose@mix.wvu.edu>
2023-09-28 17:20:46 -0400

    table working with backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from "react";
-import { DataTable } from "primereact/datatable";
-import { Column } from "primereact/column";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "./App.css";
-import { useEffect } from "react";
-import data from "./data/output.json";
+
+import Table from "./LazyTable";
+import About from "./About";
 
 function App() {
-  const [proteins] = useState(data);
-
-  console.log(proteins.length);
   return (
-    <div className="table-container">
-      <DataTable
-        globalFilterFields={["id", "name", "species", "motif"]}
-        paginator
-        removableSort
-        rows={10}
-        rowsPerPageOptions={[10, 25, 50, 100]}
-        showGridlines
-        tableStyle={{ minWidth: "50rem" }}
-        value={proteins}
-      >
-        <Column sortable filter field="id" header="ID"></Column>
-        <Column sortable filter field="name" header="Name"></Column>
-        <Column sortable filter field="motif" header="Motif"></Column>
-        <Column sortable filter field="species" header="Species"></Column>
-        <Column sortable field="domain" header="Domain"></Column>
-      </DataTable>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/table" element={<Table />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/LazyTable.jsx
@@ -0,0 +1,145 @@
+import { useState, useEffect } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+
+const baseUrl = "http://localhost:5050";
+
+const mapSort = (event) => {
+  return { [event.sortField]: event.sortOrder };
+};
+
+async function getProteins(event) {
+  let results = await fetch(`${baseUrl}/proteins/`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {
+        pageNumber: event.page,
+        pageSize: event.rows,
+        filter: event.filters,
+        sort: mapSort(event),
+      },
+    }),
+  }).then((resp) => resp.json());
+
+  return {
+    totalRecords: results.total,
+    proteins: results.proteins,
+    page: event.page,
+  };
+}
+
+export default function LazyLoadDemo() {
+  const [loading, setLoading] = useState(false);
+  const [totalRecords, setTotalRecords] = useState(0);
+  const [proteins, setProteins] = useState(null);
+  const [lazyState, setlazyState] = useState({
+    first: 0,
+    rows: 10,
+    page: 0,
+    sortField: "id",
+    sortOrder: 1,
+    filters: {
+      id: { value: "", matchMode: "contains" },
+      name: { value: "", matchMode: "contains" },
+      motif: { value: "", matchMode: "contains" },
+      species: { value: "", matchMode: "contains" },
+      domain: { value: "", matchMode: "contains" },
+    },
+  });
+
+  useEffect(() => {
+    loadLazyData();
+  }, [lazyState]);
+
+  const loadLazyData = () => {
+    setLoading(true);
+
+    //imitate delay of a backend call
+    getProteins(lazyState).then((data) => {
+      setTotalRecords(data.totalRecords);
+      setProteins(data.proteins);
+      setLoading(false);
+    });
+  };
+
+  const onPage = (event) => {
+    setlazyState(event);
+  };
+
+  const onSort = (event) => {
+    setlazyState(event);
+  };
+
+  const onFilter = (event) => {
+    event["first"] = 0;
+    setlazyState(event);
+  };
+
+  return (
+    <div className="card">
+      <DataTable
+        value={proteins}
+        lazy
+        filterDisplay="row"
+        dataKey="id"
+        paginator
+        first={lazyState.first}
+        rows={10}
+        totalRecords={totalRecords}
+        onPage={onPage}
+        onSort={onSort}
+        sortField={lazyState.sortField}
+        sortOrder={lazyState.sortOrder}
+        onFilter={onFilter}
+        filters={lazyState.filters}
+        loading={loading}
+        tableStyle={{ minWidth: "75rem" }}
+      >
+        <Column
+          field="id"
+          header="Protein ID"
+          sortable
+          filter
+          filterPlaceholder="Search"
+        />
+        <Column
+          field="name"
+          header="Name"
+          sortable
+          filter
+          filterPlaceholder="Search"
+        />
+        <Column
+          field="motif"
+          header="Motif"
+          sortable
+          filter
+          filterPlaceholder="Search"
+        />
+        <Column
+          field="species"
+          header="Species"
+          sortable
+          filter
+          filterPlaceholder="Search"
+        />
+        <Column
+          field="domain"
+          header="Domain"
+          filter
+          filterPlaceholder="Search"
+        />
+        <Column body={LinkCell}></Column>
+      </DataTable>
+    </div>
+  );
+}
+
+function LinkCell(protein) {
+  const { id } = protein;
+  const link = `https://www.uniprot.org/uniprotkb/${id}/entry#structure`;
+  return <button onClick={() => window.open(link, "_blank")}>uniprot</button>;
+}
